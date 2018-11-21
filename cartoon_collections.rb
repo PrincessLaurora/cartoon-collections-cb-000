3be3d3584d523be207ba37ef0fee77425d56fd29
Laura Felce <crepu83@yahoo.it>
2018-11-21 11:35:34 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,5 +15,5 @@ end
 
 def find_the_cheese (formaggio)
   cheese_types = ["cheddar", "gouda", "camembert"]
-    formaggio.find {|cheese| if formaggio.include?(cheese_types)}
+    formaggio.find {|cheese| formaggio.include?(cheese_types)}
 end
