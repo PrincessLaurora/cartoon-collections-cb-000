a9cefbade430fea5a659a5079ee5dc5e574f48d5
Laura Felce <crepu83@yahoo.it>
2018-11-21 10:32:21 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,7 +2,7 @@ def roll_call_dwarves(names)# code an argument here
   i = 0
   dwarves_names = []
   while i < names.length
-  dwarves_names << yield(names[i])
+  dwarves_names << yield(names[i, index])
   i = i + 1# Your code here
 end
 puts "#{index + 1}. #{i}"
