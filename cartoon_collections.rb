053b3016ac1ad54fc96fb92d64d5f5bd3bb3d6e3
Laura Felce <crepu83@yahoo.it>
2018-11-21 10:13:21 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,10 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)# code an argument here
+  i = 0
+  dwarves_names = []
+  while i < names.length
+    dwarves_names << yield(name[i])# Your code here
+end
+puts "#{index + 1}#{name}"
 end
 
 def summon_captain_planet# code an argument here
